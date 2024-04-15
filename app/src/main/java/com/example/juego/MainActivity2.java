f3171447281f58260987ffd60ea0080c4060cfaa
garridopzk <nictehagarrido45@gmail.com>
2024-04-14 20:04:12 -0600

    este es mi segundo commit

--- app/src/main/java/com/example/juego/MainActivity2.java
+++ app/src/main/java/com/example/juego/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.juego;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button comprobat;
+    EditText Respuesta;
+    EditText respuesta;
+    EditText respuestas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Respuesta=findViewById(R.id.respuesta1);
+        respuesta=findViewById(R.id.respuesta2);
+        respuestas=findViewById(R.id.respuesta3);
+        comprobat=findViewById(R.id.comprobat);
+        }
     }
-}
